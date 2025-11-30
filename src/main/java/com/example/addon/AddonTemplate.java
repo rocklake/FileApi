98138fb661e9156d5649b2ab6012e7ee4467ffba
rocklake <benrgentry@gmail.com>
2025-11-30 15:57:39 -0500

    First commit

---
+++ src/main/java/com/example/addon/AddonTemplate.java
@@ -0,0 +1,41 @@
+package com.example.addon;
+
+import com.example.addon.modules.FileApi;
+import com.mojang.logging.LogUtils;
+import meteordevelopment.meteorclient.addons.GithubRepo;
+import meteordevelopment.meteorclient.addons.MeteorAddon;
+import meteordevelopment.meteorclient.commands.Commands;
+import meteordevelopment.meteorclient.systems.hud.Hud;
+import meteordevelopment.meteorclient.systems.hud.HudGroup;
+import meteordevelopment.meteorclient.systems.modules.Category;
+import meteordevelopment.meteorclient.systems.modules.Modules;
+import org.slf4j.Logger;
+
+public class AddonTemplate extends MeteorAddon {
+    public static final Logger LOG = LogUtils.getLogger();
+    public static final Category CATEGORY = new Category("Example");
+    public static final HudGroup HUD_GROUP = new HudGroup("Example");
+
+    @Override
+    public void onInitialize() {
+        LOG.info("Starting FileApi");
+
+        // Modules
+        Modules.get().add(new FileApi());
+    }
+
+    @Override
+    public void onRegisterCategories() {
+        Modules.registerCategory(CATEGORY);
+    }
+
+    @Override
+    public String getPackage() {
+        return "com.example.addon";
+    }
+
+    @Override
+    public GithubRepo getRepo() {
+        return new GithubRepo("MeteorDevelopment", "meteor-addon-template");
+    }
+}
---
+++ src/main/java/com/example/addon/modules/FileApi.java
@@ -0,0 +1,79 @@
+//package meteordevelopment.meteorclient.systems.modules.misc;
+package com.example.addon.modules;
+
+import meteordevelopment.meteorclient.events.packets.PacketEvent;
+import meteordevelopment.meteorclient.events.world.TickEvent;
+import meteordevelopment.meteorclient.systems.modules.Categories;
+import meteordevelopment.meteorclient.systems.modules.Module;
+import meteordevelopment.orbit.EventHandler;
+
+import net.minecraft.network.packet.s2c.play.GameMessageS2CPacket;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileApi extends Module {
+
+    public FileApi() {
+        super(Categories.Misc, "FileApi", "A File API tochat.txt command.txt lastchat.txt");
+    }
+
+    @EventHandler
+    private void onTick(TickEvent.Pre event) {
+        File folder = new File("FileApi");
+        if (!folder.exists()) folder.mkdir();
+
+        if (mc.player == null || mc.world == null) return;
+
+        try {
+            File file = new File("FileApi/tochat.txt");
+            if (file.exists()) {
+                String message = new String(java.nio.file.Files.readAllBytes(file.toPath())).trim();
+                if (!message.isEmpty()) {
+
+                    mc.getNetworkHandler().sendChatMessage(message);
+
+                    java.nio.file.Files.write(file.toPath(), new byte[0]);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    @EventHandler
+    private void onTick(TickEvent.Post event) {
+        if (mc.player == null || mc.world == null || !mc.player.isAlive()) return;
+
+        File folder = new File("FileApi");
+        if (!folder.exists()) folder.mkdir();
+
+        File file = new File("FileApi/command.txt");
+        if (!file.exists()) return;
+
+        try {
+            String message = new String(java.nio.file.Files.readAllBytes(file.toPath())).trim();
+        if (!message.isEmpty()) {
+            mc.getNetworkHandler().sendChatCommand(message); // send command
+            java.nio.file.Files.write(file.toPath(), new byte[0]); // clear file
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @EventHandler
+    private void onChatPacket(PacketEvent.Receive event) {
+        if (event.packet instanceof GameMessageS2CPacket packet) {
+
+            String message = packet.content().getString();
+
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter("FileApi/lastchat.txt", false))) {
+                writer.write(message);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
